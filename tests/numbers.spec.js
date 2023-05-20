0090a3fc171eab86c24c03b73bb909448b623ba4
iago-barcelos <iago.cmpb@gmail.com>
2023-05-20 11:45:52 -0300

    numbers concluído

--- tests/numbers.spec.js
+++ tests/numbers.spec.js
@@ -15,11 +15,20 @@ const numbers = require('../src/numbers');
 */
 
 describe('2 - Implemente os casos de teste para a função `numbers`', () => {
-  it('Verifica se a função `numbers` retorna `true` quando o array contém apenas números e falso caso contrário', () => {
-    fail('Teste vazio!');
     // Escreva um teste em que a função recebe [1, 2, 3, 4, 5] e retorna true
+  it('A função circle recebe [1, 2, 3, 4, 5] e retorna true', () => {
+    expect(numbers([1, 2, 3, 4, 5])).toBe(true);
+  })
     // Escreva um teste em que a função recebe [1, 2, '3', 4, 5] e retorna false
+  it('A função circle recebe [1, 2, "3", 4, 5] e retorna false', () => {
+    expect(numbers([1, 2, '3', 4, 5])).toBe(false);
+  })  
     // Escreva um teste em que a função recebe [1, 'a', 3] e retorna false
+  it('A função circle recebe [1, "a", 3] e retorna false', () => {
+    expect(numbers([1,'a',3])).not.toBe(true);
+  })  
     // Escreva um teste em que a função recebe [' '] e retorna false
-  });
-});
+  it('A função circle recebe [" "] e retorna false', () => {
+      expect(numbers([' '])).toBe(false);  
+  })
+})
